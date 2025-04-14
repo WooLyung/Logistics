f08fe4501c40a604e04fb065854bc5db5a639a9c
WooLyung <vlfkdydtk@naver.com>
2025-04-14 20:55:02 +0900

    add LogisticsIncinerator

---
+++ Source/Logistics/Logistics/Building/Building_LogisticsIncinerator.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.Noise;
+
+namespace Logistics
+{
+    public class Building_LogisticsIncinerator : Building
+    {
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (this.IsHashIntervalTick(1000))
+                Incinerate();
+        }
+
+        private void Incinerate()
+        {
+            if (!LogisticsSystem.IsAvailableSystem(this.GetRoom()))
+                return;
+
+            List<Thing> things = new List<Thing>();
+            foreach (Thing thing in this.GetRoom().ContainedAndAdjacentThings)
+                if (thing.IsInContainer())
+                    things.Add(thing);
+
+            Thing target = things.RandomElement();
+            if (target != null)
+                target.Destroy(DestroyMode.Deconstruct);
+        }
+    }
+}
---
+++ Source/Logistics/Logistics/RoomRoleWorker/RoomRoleWorker_LogisticsIncinerationFacility.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Logistics
+{
+    public class RoomRoleWorker_LogisticsIncinerationFacility : RoomRoleWorker
+    {
+        public override float GetScore(Room room)
+        {
+            bool a = false, b = true;
+
+            List<Thing> containedAndAdjacentThings = room.ContainedAndAdjacentThings;
+            for (int i = 0; i < containedAndAdjacentThings.Count; i++)
+                if (containedAndAdjacentThings[i] is Building_LogisticsIncinerator)
+                    a = true;
+                else if (containedAndAdjacentThings[i] is Building_LogisticsSystemController)
+                    b = true;
+
+            if (a && b)
+                return 10200;
+            return 0f;
+        }
+    }
+}
